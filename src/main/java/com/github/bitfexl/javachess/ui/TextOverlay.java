9715dd2aa68f3688a2c8347758f31705b425ed31
bitfexl <54662051+bitfexl@users.noreply.github.com>
2022-12-22 16:18:03 +0100

    added better text rendering (center)

--- src/main/java/com/github/bitfexl/javachess/ui/TextOverlay.java
+++ src/main/java/com/github/bitfexl/javachess/ui/TextOverlay.java
@@ -1,11 +1,18 @@
 package com.github.bitfexl.javachess.ui;
 
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
 public class TextOverlay implements Overlay {
     private static final Font FONT = new Font("Consolas", Font.PLAIN, 30);
 
     private String text;
+
+    /**
+     * Bounds calculated by getFontForWidth().
+     */
+    private Rectangle2D fontBounds;
 
     public TextOverlay(String text) {
         this.text = text;
@@ -21,8 +28,30 @@ public class TextOverlay implements Overlay {
 
     @Override
     public void render(ChessPanel panel, Graphics2D g2d) {
-        g2d.setFont(FONT);
+        final int width = panel.getWidth();
+        final int height = panel.getHeight();
+
+        g2d.setFont(getFontForWidth(width, g2d.getFontRenderContext()));
+
+        int x = (int)(width - fontBounds.getWidth()) / 2;
+        int y = (int)(height + fontBounds.getHeight()) / 2;
+        int padding = (int)fontBounds.getHeight() / 4;
+
+        g2d.setColor(new Color(255, 255, 255, 20));
+        g2d.fillRect(x - padding, y-(int)fontBounds.getHeight(), (int)fontBounds.getWidth() + padding * 2, (int)fontBounds.getHeight());
         g2d.setColor(Color.BLACK);
-        g2d.drawString(text, 10, 40);
+        g2d.drawString(text, x, y - padding);
+    }
+
+    private Font getFontForWidth(int width, FontRenderContext fontRenderContext) {
+        Font newFont = null;
+        for (int d=0; d<20; d++) {
+            newFont = FONT.deriveFont((float)FONT.getSize() + d);
+            fontBounds = newFont.getStringBounds(text, fontRenderContext);
+            if (fontBounds.getWidth() >= width * 0.7) {
+                return newFont;
+            }
+        }
+        return newFont;
     }
 }
